01cd292a73c5af24b59b491975076b33cf2c9796
hux-x <husnainiqbal577@gmail.com>
2026-06-09 11:48:25 +0500

    .

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8000/:path*',
+        destination: 'https://api-emv.devbuns.com/:path*',
       },
     ]
   },
